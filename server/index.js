6569cdf607f78689a465398308b7d4996836d077
Steven <s3@uwaterloo.ca>
2022-07-09 22:10:05 -0400

    Fix node version

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, '../client/dist')));
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
@@ -16,7 +16,7 @@ app.get("/api", (req, res) => {
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
